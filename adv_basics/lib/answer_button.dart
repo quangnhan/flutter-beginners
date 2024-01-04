87769e687f2ef1c2c966149ff56e0e7a7ea1bbd6
quangnhan145@gmail.com <quangnhan145@gmail.com>
2024-01-04 22:58:55 +0700

    Start adv project part 3

---
+++ adv_basics/lib/answer_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton({
+    super.key,
+    required this.answerText,
+    required this.onPressed,
+  });
+
+  final String answerText;
+  final void Function() onPressed;
+
+  @override
+  Widget build(context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 40,
+        ),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(40),
+        ),
+      ),
+      child: Text(answerText),
+    );
+  }
+}
