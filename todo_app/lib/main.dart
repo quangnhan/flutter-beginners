b27f2af6a150f984d6f26e1ea1ba2b922c1e2873
quangnhan145@gmail.com <quangnhan145@gmail.com>
2024-01-22 20:11:45 +0700

    Start part 9

---
+++ todo_app/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_internals/keys/keys.dart';
+
+import 'package:flutter_internals/ui_updates_demo.dart';
+
+void main() {
+  final numbers = [1, 2, 3];
+  numbers = [4, 5];
+
+  numbers.add(4);
+
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Internals'),
+        ),
+        // body: const UIUpdatesDemo(),
+        body: const Keys(),
+      ),
+    );
+  }
+}
